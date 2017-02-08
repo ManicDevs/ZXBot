2d24e867e30721ad680c50385868c17ea7cad8a4
ManicSec <root@fbi.gov>
2017-02-08 23:08:55 +0000

    .

--- net.c
+++ net.c
@@ -18,103 +18,6 @@
 #include "xhdrs/sha256.h"
 #include "xhdrs/utils.h"
 
-ipv4_t net_local_addr(void)
-{
-	int err, val = 1, sockfd = -1;
-	
-	struct addrinfo hints;
-	struct addrinfo *result, *rp;
-	struct timeval tv;
-	
-	struct sockaddr in_addr;
-	socklen_t in_len = sizeof(in_addr);
-	
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;     	// Return IPv4 and IPv6 choices
-	hints.ai_socktype = SOCK_DGRAM;		// We want a UDP socket
-	hints.ai_flags = AI_PASSIVE;		// All interfaces
-	
-	err = getaddrinfo("8.8.8.8", "53", &hints, &result);
-	if(err < 0)
-	{
-#ifdef DEBUG
-		util_msgc("Error", "Failed to Getaddrinfo!");
-#endif
-		return -1;
-	}
-	
-	for(rp = result; rp != NULL; rp = rp->ai_next)
-	{
-		sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-		if(sockfd < 0)
-			continue;
-		
-		if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(int)) < 0)
-		{
-#ifdef DEBUG
-			util_msgc("Warning", "Unable to set SO_REUSEADDR: %s",  
-				strerror(errno));
-#endif
-		}
-		
-		tv.tv_sec = 5;
-		tv.tv_usec = 0;
-		
-		if(setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, 
-						(struct timeval*)&tv, sizeof(struct timeval)))
-		{
-#ifdef DEBUG
-			util_msgc("Warning", "Unable to set SO_RCVTIMEO: %s",  
-				strerror(errno));
-#endif
-		}
-		
-		if(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, 
-						(struct timeval*)&tv, sizeof(struct timeval)))
-		{
-#ifdef DEBUG
-			util_msgc("Warning", "Unable to set SO_RCVTIMEO: %s",  
-				strerror(errno));
-#endif
-		}
-		
-		if((err = connect(sockfd, rp->ai_addr, rp->ai_addrlen)) < 0)
-		{
-			close(sockfd);
-			sockfd = -1;
-		}
-		
-		if(err == 0)
-			break;
-		//close(sockfd);
-	}
-	
-	freeaddrinfo(result);
-	
-	if(sockfd < 0)
-	{
-#ifdef DEBUG
-		util_msgc("Error", "Failed to connect: %s", strerror(errno));
-#endif
-		return -1;
-	}
-	
-	if(net_set_nonblocking(sockfd) < 0)
-	{
-#ifdef DEBUG
-		util_msgc("Error", "Failed to make socket nonblocking: %s", 
-			strerror(errno));
-#endif
-		return -1;
-	}
-	
-	getsockname(sockfd, &in_addr, &in_len);
-	
-	close(sockfd);
-	
-	return ((struct sockaddr_in*)&in_addr)->sin_addr.s_addr;
-}
-
 int net_fdsend(int sockfd, int type, char *buffer)
 {
 	struct Packet pkt;
@@ -312,4 +215,20 @@ int net_connect(const char *addr, const char *portno, int protocol)
 #endif
 	
 	return sockfd;
+}
+
+ipv4_t net_local_addr(void)
+{
+	int sockfd = -1;
+	
+	struct sockaddr in_addr;
+	socklen_t in_len = sizeof(in_addr);
+	
+	sockfd = net_connect("8.8.8.8", "53", IPPROTO_UDP);
+	
+	getsockname(sockfd, &in_addr, &in_len);
+	
+	close(sockfd);
+	
+	return ((struct sockaddr_in*)&in_addr)->sin_addr.s_addr;
 }
